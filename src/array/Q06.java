3d9b84abf1a195100e0a387937697dced6e9e017
eeesnghyun <szhyun2002@gmail.com>
2023-04-09 17:03:41 +0900

    Array(1,2차원 배열) - 뒤집은 소수 문제 풀이

--- src/array/Q06.java
+++ src/array/Q06.java
@@ -1,5 +1,8 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -14,18 +17,53 @@ import java.util.Scanner;
  */
 public class Q06 {
 
-    public static String solution(String str) {
-        String result = "";
+    private static boolean isPrime(int num) {
+        boolean[] arr = new boolean[num];
 
+        //1은 소수가 아니기 때문에 제외
+        for (int i = 1; i < num; i++) arr[i] = true;
 
-        return result;
+        //2부터
+        for (int i = 2; i < num; i++) {
+            for (int j = (i * 2) - 1; j < num; j = j + i) {
+                arr[j] = false;
+            }
+        }
+
+        return arr[num - 1];
+    }
+
+    public static ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        for (int number : arr) {
+            int tmp = number;
+            int res = 0;
+
+            while (tmp > 0) {
+                int t = tmp % 10;
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+
+            if (isPrime(res)) resultList.add(res);
+        }
+
+        return resultList;
     }
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        String str = kb.next();
+        int cnt = kb.nextInt();
+        int[] arr = new int[cnt];
 
-        System.out.println(solution(str));
+        for (int i = 0; i < cnt; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        for (int number : solution(arr)) {
+            System.out.printf(number + " ");
+        }
     }
 }
