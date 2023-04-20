4b96bb2bbfd9337410469ad0f76ed7a8a76b947d
wwl-lsh <lsh@winwinlab.co.kr>
2023-04-20 16:47:58 +0900

    Array(1,2차원 배열) - 봉우리 문제 풀이

--- src/array/Q10.java
+++ src/array/Q10.java
@@ -14,9 +14,30 @@ import java.util.Scanner;
  */
 public class Q10 {
 
-    public static String solution(String str) {
-        String result = "";
+    public static int solution(int cnt, int[][] array) {
+        int result = 0;
 
+        for (int i = 0; i < cnt; i++) {
+            for (int j = 0; j < cnt; j++) {
+                boolean isBig = true;
+                int top = 0, left = 0;
+                int bottom = 0, right = 0;
+
+                if (i - 1 >= 0) top = array[i-1][j];
+                if (j - 1 >= 0) left = array[i][j-1];
+                if (i + 1 < cnt) bottom = array[i+1][j];
+                if (j + 1 < cnt) right = array[i][j+1];
+
+                if (array[i][j] <= top
+                    || array[i][j] <= left
+                    || array[i][j] <= bottom
+                    || array[i][j] <= right) {
+                    isBig = false;
+                }
+
+                if (isBig) result++;
+            }
+        }
 
         return result;
     }
@@ -24,8 +45,15 @@ public class Q10 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
 
-        String str = kb.next();
+        int cnt = kb.nextInt();
+        int[][] array = new int[cnt][cnt];
 
-        System.out.println(solution(str));
+        for (int i = 0; i < cnt; i++) {
+            for (int j = 0; j < cnt; j++) {
+                array[i][j] = kb.nextInt();
+            }
+        }
+
+        System.out.println(solution(cnt, array));
     }
 }
